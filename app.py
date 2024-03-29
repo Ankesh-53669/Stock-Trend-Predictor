59bd707168d250644af380b4a2f2d9a5ffcfd49b
Ankesh pandey <119523199+Ankesh-53669@users.noreply.github.com>
2024-03-29 15:02:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data
+
 from keras.models import load_model
 from sklearn.metrics import r2_score
 import yfinance as yf
